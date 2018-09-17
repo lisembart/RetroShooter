409d14eac7dd51d1e45c3f4cc8833a33663e5517
Bartek Lis <lisembart97@gmail.com>
2018-09-17 23:46:31 +0200

    Created TextMeshPro fonts

--- Assets/Scripts/NPCs/NPC.cs
+++ Assets/Scripts/NPCs/NPC.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC : MonoBehaviour 
 {
+	[SerializeField] private string npcName;
+
+	[Header("UI")]
+	public bool showingBars = false;
+	[SerializeField] private GameObject UICanvas;
+	[SerializeField] private Text nameText;
 
 	void Start () 
 	{
@@ -13,6 +20,20 @@ public class NPC : MonoBehaviour
 
 	void Update () 
 	{
-		
+		nameText.text = npcName;
+
+		if(showingBars)
+		{
+			UICanvas.SetActive(true);
+		} else 
+		{
+			UICanvas.SetActive(false);
+		}
+	}
+
+	public void SetBars(bool value)
+	{
+		Debug.Log("SETTING VALUE");
+		showingBars = value;
 	}
 }
